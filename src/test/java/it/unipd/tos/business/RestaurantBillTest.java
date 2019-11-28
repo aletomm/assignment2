36a262b9a3c7d74774704e460d997642f34b1b62
Alessandro <tommasinalessandro@gmail.com>
2019-11-28 10:13:50 +0100

    Inizializzata classe di test

---
+++ src/test/java/it/unipd/tos/business/RestaurantBillTest.java
@@ -0,0 +1,14 @@
+package it.unipd.tos.business;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RestaurantBillTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
